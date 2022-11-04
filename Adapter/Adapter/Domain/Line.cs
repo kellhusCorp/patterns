d8bcbade80ef30b2255d6bd24380694d0d80adb1
kellhusCorp <alkan_7@mail.ru>
2022-11-04 17:11:37 +0400

    added caching to adapter.

---
+++ Adapter/Adapter/Domain/Line.cs
@@ -0,0 +1,12 @@
+﻿namespace Adapter;
+
+public class Line
+{
+    public Point Start, End;
+
+    public Line(Point start, Point end)
+    {
+        Start = start;
+        End = end;
+    }
+}
---
+++ Adapter/Adapter/Domain/Point.cs
@@ -0,0 +1,47 @@
+﻿namespace Adapter;
+
+public class Point : IEquatable<Point>
+{
+    public int X, Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+    }
+
+    public bool Equals(Point? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Point) obj);
+    }
+
+    public static bool operator ==(Point? left, Point? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Point? left, Point? right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+}
