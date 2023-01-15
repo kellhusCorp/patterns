8127802ad6d4d1f1218f973968b76f945510f4b3
kellhusCorp <alkan_7@mail.ru>
2023-01-15 21:20:39 +0400

    Добавил пример для null object

---
+++ NullObject/Program.cs
@@ -0,0 +1,14 @@
+﻿using NullObject;
+using NullObject.Common;
+
+static class Program
+{
+    static void Main()
+    {
+        var ba = new BankAccount(new ConsoleLog());
+        ba.Deposit(100);
+        ba.Deposit(300);
+        ba.Withdraw(300);
+        ba.Withdraw(1000);
+    }
+}
