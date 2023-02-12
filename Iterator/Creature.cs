f06cce29ae44f9447b4640f5b4148ac61303ac0a
kellhusCorp <alkan_7@mail.ru>
2023-02-12 15:06:41 +0400

    ~

---
+++ Iterator/Creature.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace Iterator;
+
+// Array backed properties
+
+public class Creature : IEnumerable<int>
+{
+    private int[] stats = new int[3];
+
+    private const int StrengthIndex = 0;
+
+    public int Strength
+    {
+        get => stats[StrengthIndex];
+        set => stats[StrengthIndex] = value;
+    }
+
+    private const int AgilityIndex = 1;
+
+    public int Agility
+    {
+        get => stats[AgilityIndex];
+        set => stats[AgilityIndex] = value;
+    }
+
+    private const int IntelligenceIndex = 2;
+
+    public int Intelligence
+    {
+        get => stats[IntelligenceIndex];
+        set => stats[IntelligenceIndex] = value;
+    }
+
+    public double SumOfStats => stats.Sum();
+
+    public double AverageStats => stats.Average();
+
+    public double MaxStats => stats.Max();
+    public IEnumerator<int> GetEnumerator()
+    {
+        return stats.AsEnumerable().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            if (index <= 0) throw new ArgumentOutOfRangeException(nameof(index));
+            return stats[index];
+        }
+        set
+        {
+            if (index <= 0) throw new ArgumentOutOfRangeException(nameof(index));
+            stats[index] = value;
+        }
+    }
+}
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -2,18 +2,18 @@
 
 internal static class Program
 {
-    static void Main()
+    private static void Main()
     {
         //    1
         //   / \
         //  2   3
         // /\   /\
         //4  5 6  7
-        
+
         var leftSubTree = new Node<int>(2, new Node<int>(4), new Node<int>(5));
 
         var rightSubTree = new Node<int>(3, new Node<int>(6), new Node<int>(7));
-        
+
         var root = new Node<int>(1, leftSubTree, rightSubTree);
 
         var iterator = new InOrderIterator<int>(root);
@@ -22,9 +22,20 @@ internal static class Program
 
         Console.WriteLine(string.Join(",", tree.InOrder.Select(x => x.Value)));
 
-        foreach (var node in tree)
+        foreach (var node in tree) Console.WriteLine(node.Value);
+
+        var creature = new Creature();
+        creature.Strength = 10;
+        creature.Intelligence = 11;
+        creature.Agility = 12;
+
+        Console.WriteLine($"Max stat = {creature.MaxStats}, " +
+                          $"Avg stat = {creature.AverageStats}," +
+                          $"Sum stat = {creature.SumOfStats}");
+
+        foreach (var stats in creature)
         {
-            Console.WriteLine(node.Value);
+            Console.WriteLine(stats);
         }
     }
 }
