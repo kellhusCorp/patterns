f34c0964189a89e67f6c6eca543843cb831a7390
kellhusCorp <alkan_7@mail.ru>
2023-02-12 14:09:32 +0400

    Добавил тип итератора для обхода типа inOrder дерева.

---
+++ Iterator/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace Iterator;
+
+public class Node<T>
+{
+    public T Value;
+
+    public Node<T> Left;
+    public Node<T> Right;
+    
+    public Node<T> Parent;
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public Node(T value, Node<T> left, Node<T> right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+
+        Left.Parent = this;
+        Right.Parent = this;
+    }
+}
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,7 +1,26 @@
-﻿static class Program
+﻿namespace Iterator;
+
+internal static class Program
 {
     static void Main()
     {
+        //    1
+        //   / \
+        //  2   3
+        // /\   /\
+        //4  5 6  7
         
+        var leftSubTree = new Node<int>(2, new Node<int>(4), new Node<int>(5));
+
+        var rightSubTree = new Node<int>(3, new Node<int>(6), new Node<int>(7));
+        
+        var root = new Node<int>(1, leftSubTree, rightSubTree);
+
+        var iterator = new InOrderIterator<int>(root);
+
+        while (iterator.MoveNext())
+        {
+            Console.WriteLine(iterator.Current.Value);
+        }
     }
 }
