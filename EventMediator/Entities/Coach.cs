1845b48eee7a701cd4105c5dcae33ef2a15ab236
kellhusCorp <alkan_7@mail.ru>
2023-03-13 22:57:59 +0400

    2. Добавил пример при использовании событий.

---
+++ EventMediator/Entities/Coach.cs
@@ -0,0 +1,21 @@
+﻿using EventMediator.Mediators;
+
+namespace EventMediator.Entities;
+
+public class Coach
+{
+    private Game game;
+
+    public Coach(Game game)
+    {
+        this.game = game;
+
+        game.Events += (sender, args) =>
+        {
+            if (args is PlayerScoredEventArgs scored && scored.GoalsScoredSoFar < 3)
+            {
+                Console.WriteLine($"Тренер сказал: круто {scored.PlayerName}!");
+            }
+        };
+    }
+}
---
+++ EventMediator/Mediators/Game.cs
@@ -0,0 +1,16 @@
+﻿using EventMediator.EventArgs;
+
+namespace EventMediator.Mediators;
+
+/// <summary>
+/// This is mediator.
+/// </summary>
+public class Game
+{
+    public event EventHandler<GameEventArgs> Events;
+
+    public void Fire(GameEventArgs args)
+    {
+        Events?.Invoke(this, args);
+    }
+}
