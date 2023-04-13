24ef7586d457f0c342181e1ba6d3d224a046edf0
kellhusCorp <alkan_7@mail.ru>
2023-04-13 22:02:24 +0400

    Added the example of dynamic decorator.

---
+++ DynamicDecorator/ColorShape.cs
@@ -0,0 +1,19 @@
+﻿namespace DynamicDecorator;
+
+public class ColorShape : Shape
+{
+    private Shape shape;
+
+    private string color;
+
+    public ColorShape(Shape shape, string color)
+    {
+        this.shape = shape;
+        this.color = color;
+    }
+
+    public override string AsString()
+    {
+        return $"{shape.AsString()} has the color {color}";
+    }
+}
---
+++ DynamicDecorator/TransparentShape.cs
@@ -0,0 +1,19 @@
+﻿namespace DynamicDecorator;
+
+public class TransparentShape : Shape
+{
+    private Shape shape;
+
+    private float transparency;
+
+    public TransparentShape(Shape shape, float transparency)
+    {
+        this.shape = shape;
+        this.transparency = transparency;
+    }
+
+    public override string AsString()
+    {
+        return $"{shape.AsString()} has {transparency * 100.0f}% {nameof(transparency)}";
+    }
+}
