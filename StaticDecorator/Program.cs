44d8c420b44f8c96e3d085ee1496be910d6efe00
kellhusCorp <alkan_7@mail.ru>
2023-04-27 23:26:23 +0400

    Added the example of static decorator

---
+++ StaticDecorator/Program.cs
@@ -0,0 +1,14 @@
+﻿using DynamicDecorator;
+
+namespace StaticDecorator;
+
+internal static class StaticDecoratorProgram
+{
+    private static void Main()
+    {
+        var blueCircle = new ColoredShape<Circle>("blue");
+        Console.WriteLine(blueCircle.AsString());
+
+        var blackHalfCircle = new TransparentShape<ColoredShape<Circle>>(0.5f);
+    }
+}
