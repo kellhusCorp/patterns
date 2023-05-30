1847614f7378f5307e9a44a97b2e88caf9029bc2
kellhusCorp <alkan_7@mail.ru>
2023-05-30 22:36:02 +0400

    2. Added types

---
+++ Memento/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Memento;
+
+public class BankAccount
+{
+    private int _balance;
+
+    public BankAccount(int balance)
+    {
+        _balance = balance;
+    }
+
+    public Memento Deposit(int amount)
+    {
+        _balance += amount;
+        return new Memento(_balance);
+    }
+
+    public void Restore(Memento m)
+    {
+        _balance = m.Balance;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(_balance)}: {_balance}";
+    }
+}
