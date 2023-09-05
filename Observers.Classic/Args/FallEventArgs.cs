bd4a46fd79402199912ebb7ea867924ac590e8ab
kellhusCorp <kellhus.corp@gmail.com>
2023-09-06 00:33:26 +0400

    Added example of classic observer on events

---
+++ Observers.Classic/Args/FallEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Observers.Classic.Args
+{
+    public class FallEventArgs : EventArgs
+    {
+        public FallEventArgs(DateTime happenDate)
+        {
+            HappenDate = happenDate;
+        }
+
+        public DateTime HappenDate { get; set; }
+    }
+}
---
+++ Observers.Classic/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using Observers.Classic.Args;
+
+namespace Observers.Classic.Entities
+{
+    public class Person
+    {
+        public event EventHandler<FallEventArgs> Fall; 
+    
+        public void GoUpstairs()
+        {
+            Fall?.Invoke(this, new FallEventArgs(DateTime.Now));
+        }
+    }
+}
