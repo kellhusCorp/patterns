0aa644d6b615bf81a062be34c18f7d02faa9fa84
Roman <69262893+kellhusCorp@users.noreply.github.com>
2023-10-08 16:04:21 +0400

    Merge pull request #21
    * Added example of classic observer on events
    
    * added example of observer on reactive extensions
    
    * Added the example of observable properties.

---
+++ Observers.Classic/Program.cs
@@ -0,0 +1,22 @@
+﻿using Observers.Classic.Args;
+using Observers.Classic.Entities;
+
+namespace Observers.Classic
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var person = new Person();
+        
+            person.Fall += PersonOnFall;
+        
+            person.GoUpstairs();
+        }
+
+        private static void PersonOnFall(object? sender, FallEventArgs e)
+        {
+            Console.WriteLine($"Person has fallen {e.HappenDate}");
+        }
+    }
+}
---
+++ Observers.Interfaces/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Reactive.Linq;
+
+public class Event
+{
+    
+}
+
+public class FallDownEvent : Event
+{
+    
+}
+
+public class Person : IObservable<Event>
+{
+    private readonly HashSet<Subscription> _subscriptions = new();
+    
+    public void FallDown()
+    {
+        foreach (Subscription subscription in _subscriptions)
+        {
+            subscription.Observer.OnNext(new FallDownEvent());
+        }
+    }
+    
+    public IDisposable Subscribe(IObserver<Event> observer)
+    {
+        var sub = new Subscription(this, observer);
+        _subscriptions.Add(sub);
+        return sub;
+    }
+
+    private class Subscription : IDisposable
+    {
+        private Person _person;
+
+        public IObserver<Event> Observer;
+        
+        public Subscription(Person person, IObserver<Event> observer)
+        {
+            _person = person;
+            Observer = observer;
+        }
+
+        public void Dispose()
+        {
+            _person._subscriptions.Remove(this);
+        }
+    }
+}
+
+public class Program
+{
+    public Program()
+    {
+        var person = new Person();
+        
+        person.OfType<FallDownEvent>()
+            .Subscribe(_ => Console.WriteLine("We need a doctor"));
+        
+        person.FallDown();
+    }
+    
+    public static void Main()
+    {
+        new Program();
+    }
+
+    public void OnCompleted()
+    {
+        
+    }
+
+    public void OnError(Exception error)
+    {
+        
+    }
+
+    public void OnNext(Event value)
+    {
+        if (value is FallDownEvent)
+        {
+            Console.WriteLine("Person has fallen down");
+        }
+    }
+}
