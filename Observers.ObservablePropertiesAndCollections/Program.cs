6eae40c2b8d70dc2375dce22b4ed965cf5ca1995
kellhusCorp <kellhus.corp@gmail.com>
2023-10-08 16:03:21 +0400

    Added the example of observable properties.

---
+++ Observers.ObservablePropertiesAndCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class Person
+    : INotifyPropertyChanged
+{
+    private int _age;
+
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (value == _age) return;
+            _age = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public event PropertyChangingEventHandler? PropertyChanging;
+}
+
+internal class Program
+{
+    public static void Main()
+    {
+        var person = new Person();
+        person.PropertyChanged += PersonOnPropertyChanged;
+        person.Age = 10;
+    }
+
+    private static void PersonOnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        var person = sender as Person;
+        Console.WriteLine($"{e.PropertyName} {person.Age}");
+    }
+}
